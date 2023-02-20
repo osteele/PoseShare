a3039d19daf4a0e538b1f104015e5ffc38a81b7c
Oliver Steele <steele@osteele.com>
2023-02-20 23:12:02 +0800

    Remove some unused functions

--- src/performers.ts
+++ src/performers.ts
@@ -76,47 +76,6 @@ export function updatePerformerData(performerData: Performer[]) {
   performerData.forEach((person) => updatePersonPose(person));
 }
 
-// function createPartnerSelector(p5: P5) {
-//   const showAllString = "Show All";
-
-//   // get the name of the performer as an option for the selection element
-//   function getOptionName({ name, isSelf }: { name: string; isSelf: boolean }) {
-//     if (isSelf) name += " (self)";
-//     return name;
-//   }
-
-//   p5.createDiv("Partner:").class("label").parent("partner-container");
-//   const sel = p5.createSelect().parent("partner-container");
-//   sel.option(showAllString);
-
-//   sel.changed(() => {
-//     const item = sel.value();
-//     if (item === showAllString) {
-//       partnerId = null;
-//     } else {
-//       const partner = performers.find(
-//         (person) => getOptionName(person) === item
-//       );
-//       partnerId = partner ? partner.id : null;
-//     }
-//   });
-
-//   // TODO: use this when the list of partners or their names change, instead of
-//   // on mouse entry
-//   sel.mouseMoved(() => {
-//     sel.elt.innerHTML = "";
-//     sel.option(showAllString);
-//     sel.selected(showAllString);
-//     for (const person of getOtherPerformers()) {
-//       const optionName = getOptionName(person);
-//       sel.option(optionName);
-//       if (person.id === partnerId) {
-//         sel.selected(optionName);
-//       }
-//     }
-//   });
-// }
-
 export function getPerformers({ includeSelf }: { includeSelf?: boolean } = {}) {
   const activePerformers = performers.filter(
     ({ pose, connected }) => pose && connected
@@ -137,34 +96,6 @@ export function getPerformers({ includeSelf }: { includeSelf?: boolean } = {}) {
   return result;
 }
 
-// function getOtherPerformers() {
-//   return performers.filter(({ isSelf, pose }) => !isSelf && pose);
-// }
-
 export function getOwnRecord() {
   return performers.find(({ isSelf, pose }) => isSelf && pose)!;
 }
-
-// function getPerformersForGallery() {
-//   const syntheticPerformersMatch = document.location.hash.match(
-//     /synthetics(?:=(\d+))?/
-//   );
-//   let synthesizePerformerTotal =
-//     syntheticPerformersMatch === null
-//       ? 0
-//       : Number(syntheticPerformersMatch[1] || 0);
-//   let activePerformers = performers;
-//   if (synthesizePerformerTotal) {
-//     // synthesize some more performers, for debugging purposes
-//     activePerformers = [...activePerformers];
-//     let srcIndex = 0;
-//     while (
-//       activePerformers.length > 0 &&
-//       activePerformers.length < synthesizePerformerTotal
-//     ) {
-//       const src = activePerformers[srcIndex++];
-//       activePerformers.push({ ...src, id: src.id + "-1", hue: src.hue + 30 });
-//     }
-//   }
-//   return activePerformers;
-// }
